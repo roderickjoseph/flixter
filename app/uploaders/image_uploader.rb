ecbb6928a09b6f99529adb02ad05290787e8ded0
roderickjoseph <rjgauvin@gmail.com>
2017-04-08 01:15:02 +0000

    Styled course show page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fill: [800, 350]
+  process resize_to_fit: [800, 350]
   #
   # def scale(width, height)
   #   # do something
